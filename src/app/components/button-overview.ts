d9d133f8cd0232bb4ba289c658a5c4c74bd2e2db
Guilherme Florêncio <guilherme.florencio@sergipetec.org.br>
2022-11-21 10:52:00 -0300

    Inicio do Projeto

---
+++ src/app/components/button-overview.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic buttons
+ */
+@Component({
+  selector: 'button-overview',
+  templateUrl: 'button-overview.html',
+  styleUrls: ['button-overview.css']
+})
+export class ButtonOverviewExample {}
